5bade5288c81b1f45096c66173f664f5e375d71a
Patricia S. Gracia Artero <patryechelon@gmail.com>
2022-06-09 13:16:28 +0200

    Objetos arreglados y Script pointsScore
    Arreglados los objetos deformados por respawn
    arreglado el script para usar las luces

--- Assets/Scripts/ScoreAdmin.cs
+++ Assets/Scripts/ScoreAdmin.cs
@@ -8,27 +8,49 @@ using UnityEngine.UI;
 public class ScoreAdmin: MonoBehaviour
 {
     [SerializeField] private Transform Object;
-    //[SerializeField] private Transform respawnPointObj;
-    public string Tag;
+    public GameObject RightLight;
+    public GameObject WrongLight;
+    public GameObject NeutralLight;
 
-    public UnityEvent Right;
-    public UnityEvent Wrong;
-    public UnityEvent Neutral;
+    public string Tag;
+    //public string Tag2;
+
+    //public UnityEvent Right;
+    //public UnityEvent Wrong;
+    //public UnityEvent Neutral;
+
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(Tag)) 
         {
             ScoreManager.instance.AddPoint();
-            Right.Invoke();
+            //Right.Invoke();
+            Right2();
         }
         else if (other.CompareTag("Player")){}
         else if (other.CompareTag("Untagged")){}
         else
         {
+            //else if (Object.CompareTag(Tag2)==(other.CompareTag(Tag)){Neutral.Invoke();}
             ScoreManager.instance.RemovePoint();
-            Wrong.Invoke();
-        }    
+            //Wrong.Invoke();
+            Wrong2();
+
+        }
+    }
+
+    private void Right2()
+    {
+        RightLight.SetActive(true);
+    }
+    private void Wrong2()
+    {
+        WrongLight.SetActive(true);
+    }
+    private void Neutral2()
+    {
+        NeutralLight.SetActive(true);
     }
 }
 
